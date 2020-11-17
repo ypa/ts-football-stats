be6836c27d13a355921eefec43fa9a9ff606b597
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-17 06:53:26 -0800

    Refactor out custom parsing logic out to mapRow helper function

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -19,17 +19,18 @@ export class CsvFileReader {
         return row.split(',');
       }
     )
-    .map((row: string[]): MatchData => {
-      return [
-        dateStringToDate(row[0]),
-        row[1],
-        row[2],
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResult, // Type assertion
-        row[6]
-      ];
-    });
+    .map(this.mapRow);
+  }
 
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult, // Type assertion
+      row[6]
+    ];
   }
 }
