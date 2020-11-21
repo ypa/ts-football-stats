40cce333dda941cfcc43106e37d5f26a944c77ad
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-21 07:49:24 -0800

    Also refactor MatchReader with static factory fromCsv and clean up index accordingly

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
-import { WinsAnalysis } from './analyzers/WinAnalysis';
 import { Summary } from './Summary';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in object
-// that satisfies DataReader interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-// matchReader.matches
-
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
